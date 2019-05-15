361f7cdb177d1ae99b14adbaa3da88db943dd247
slimykat <b05902011@ntu.edu.tw>
2019-05-15 10:11:34 +0800

    5/15

--- browser/COOP_found.sol
+++ browser/COOP_found.sol
@@ -1,18 +1,33 @@
 pragma solidity >=0.4.22 <0.6.0;
 
-contract coop_found {
-
+contract coop_find {
+    
     struct Mission {
         uint coins;
         address payable host;
-        address payable coop;
         uint description; /// bitmap of mission catagory
         bool status;
+        address payable [] queue;
+        uint queue_last;
+        uint queue_next;
     }
+    
+    function dequeue(uint mission_num) public returns (address payable data) {
+        
+        require(mission[mission_num].queue_last > mission[mission_num].queue_next, "queue empty"); 
+        // non-empty queue
+
+        data = mission[mission_num].queue[mission[mission_num].queue_next];
+        delete mission[mission_num].queue[mission[mission_num].queue_next];
+        
+        mission[mission_num].queue_next += 1;
+    }
+    
     struct Account{
         uint has_issue;
-        uint in_mission;
-        bool exist;
+        uint in_task;
+        uint finished_issue;
+        uint finished_task;
     }
     
     address chairperson;
@@ -21,37 +36,32 @@ contract coop_found {
     
     uint num_of_missions;
     uint max_of_missions;
+    uint max_of_issues;
+    uint max_of_tasks;
+    
     /// initiation, set a limited list of missions
-    constructor() public {
+    constructor(uint _max_of_missions, uint _max_of_issues, uint _max_of_tasks) public {
         chairperson = msg.sender;
         num_of_missions = 0;
-        max_of_missions = 0;
-    }
-
-    function set_queue(uint _max_of_missions)public{
-        require(msg.sender == chairperson, "not enough authority");
         max_of_missions = _max_of_missions;
+        max_of_issues = _max_of_issues;
+        max_of_tasks = _max_of_tasks;
     }
-
-    function build_account() public{
-        require(account[msg.sender].exist == false, "account already exist");
-        account[msg.sender].has_issue = 0;
-        account[msg.sender].in_mission = 0;
-        account[msg.sender].exist = true;
-    }
-
+    
     /// give an issue, find coops
-    function Issue(uint descript, uint coins) public returns(uint){
+    function Issue(uint descript, uint coins) public payable returns(uint){
         require(num_of_missions < max_of_missions, "not enough queue space");
-        require(account[msg.sender].has_issue < 10, "too many issues");
+        require(account[msg.sender].has_issue < max_of_issues, "too many issues");
+        require(msg.value == 2*coins);
         
         uint mission_num = descript % max_of_missions;
         
         while(mission[mission_num].status == true){
-            mission_num = (mission_num + 1) % max_of_missions;
+            mission_num = (mission_num) % max_of_missions;
         }
-        
-        Mission memory new_mission = Mission(coins, msg.sender, msg.sender, descript, true);
+        address payable [] memory new_queue;
+        Mission memory new_mission = Mission(
+            coins, msg.sender, descript, true, new_queue, 0, 0);
         mission[mission_num] = new_mission;
         num_of_missions ++;
         account[msg.sender].has_issue++;
@@ -60,54 +70,59 @@ contract coop_found {
         return(mission_num);
     }
     
-    /// find a mission, become a coop
-    function Assign(uint mission_num)public{
-        require(mission[mission_num].status == true, "mission DNE");
-        require(mission[mission_num].coop == mission[mission_num].host, "already taken");
-        require(account[msg.sender].in_mission < 10, "has too much missions");
-        
-        mission[mission_num].coop = msg.sender;
-        account[msg.sender].in_mission ++;
-    }
+    event coop_found(uint _mission_num, address _who);
     
-    function Getinfo(uint mission_num)public view returns(uint coin, bool status, uint descript, address whose, address towho){
-        if(mission_num > num_of_missions){
-            status = false;
-            descript = 0;
-        }
-        else{
-            status = mission_status[mission_num];
-            descript = mission[mission_num].description;
-            whose = mission[mission_num].host;
-            towho = mission[mission_num].coop;
-            coin = mission[mission_num].coins;
-        }
+    /// find a mission, become a coop
+    function Assign(uint mission_num) public{
+        require(mission[mission_num].status == true, "mission DNE");
+        require(account[msg.sender].in_task < max_of_tasks, "has too much missions");
+        
+        account[msg.sender].in_task ++;
+        mission[mission_num].queue.push(msg.sender);
+        mission[mission_num].queue_last++;
+        emit coop_found(mission_num, msg.sender);
+    }
+   
+    function Getinfo(uint mission_num)public view returns(
+        uint coin, bool stat, uint descript, address whose, uint queue_length){
+        require(mission[mission_num].status == true, "mission DNE");
+           
+        stat = mission[mission_num].status;
+        descript = mission[mission_num].description;
+        whose = mission[mission_num].host;
+        queue_length = mission[mission_num].queue_last - mission[mission_num].queue_next;
+        coin = mission[mission_num].coins;
+
     }
 
-    function Search(uint nodes, address towho, uint filter_number)public returns(uint num){
-        ///algorithm**
-        /// break keywords into nodes and store in bitmap
-        /// calculate dist for each possible missions
-        /// return the missions that is under the filter_number
+    event coop_paid(address _coop, bool _accept_or_not);
+    
+    function pay_coop(uint mission_num, bool accept_or_not)public payable{
+        require(mission[mission_num].host == msg.sender);
+        
+        address payable towho = dequeue(mission_num);
+        if(accept_or_not){
+            msg.value == mission[mission_num].coins;
+        }
+        towho.transfer(msg.value);
+        account[towho].in_task--;
+        if(accept_or_not) {
+            account[towho].finished_task++;
+        }
+        emit coop_paid(towho, accept_or_not);
     }
     
-    function coop_found(uint mission_num, address )public payable{
-        require(mission[mission_num].host == msg.sender);
-        require(mission[mission_num].coins <= msg.value);
-        
-        
-        /*
-        Mission memory mission = stores[missionOwner[_missionId]][_missionId];
-        mission.solver = applicantById[_applicantId].addr;
-        stores[missionOwner[_missionId]][_missionId] = mission;
-        Escrow escrow = (new Escrow).value(msg.value)(_missionId, msg.sender, mission.solver, arbiter);
-        missionEscrow[_missionId] = address(escrow);
-        */
-        
-    }
-    function coop_deny(uint mission_num)public{
-        ///not sure if needed
-        ///require(mission_num[mission_num].host == msg.sender);
-        ///mission[mission_num].coop = null;
+    function mission_abort(uint mission_num)public{
+        require(mission[mission_num].status == true, "mission DNE");
+        require(mission[mission_num].host == msg.sender, "account does not possess the mission");
+        require(mission[mission_num].queue_next == mission[mission_num].queue_last,
+            "queue not cleared");
+        msg.sender.transfer(mission[mission_num].coins *2);
+        mission[mission_num].status = false;
+        /// delete mission[mission_num];
+        account[msg.sender].has_issue--;
+        account[msg.sender].finished_issue++;
+        num_of_missions --;
     }
 }
+
